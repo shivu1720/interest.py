a7b542f97e717ec856f26251135a71aee95d161a
Basu2006 <basavarajholal26@gmail.com>
2026-01-15 14:49:25 +0530

    my first commit

---
+++ simpleinterest.py
@@ -0,0 +1,5 @@
+principal = float(input("Enter principal: ")) 
+rate = float(input("Enter rate: ")) 
+time = float(input("Enter time: ")) 
+si = (principal * rate * time) / 100 
+print("Simple Interest:", si) 
